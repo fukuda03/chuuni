240730e14bc59bc3f7f699fae5bdb9278536cca4
shouta <svtd9978@yahoo.co.jp>
2019-05-08 22:43:16 +0900

    ちょっとだけレイアウト変えた

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,18 @@
 class TopController < ApplicationController
   def new
-    @topics = Topic.all
+    @topics = Topic.all.order(created_at: :desc)
   end
 
+  def hashtag
+  @user = current_user
+  hash_name = params[:name]
+  @topics = Topic.where('description LIKE ?', "%##{hash_name}%")
+end
+
+
+private
+ def topic_params
+   params.require(:topic).permit(:image, :description)
+ end
 
 end
